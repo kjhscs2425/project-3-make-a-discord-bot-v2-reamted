fd3ae6a6774bcd1e0656d234d6604a7574b60bfd
reamted <nroussos2028@kehillahstudent.org>
2025-02-12 11:55:32 -0800

    test

--- my_bot.py
+++ my_bot.py
@@ -104,7 +104,9 @@ def respond(user_message, user_name):
     return f"""what do you think i am, some actaully competent AI, naaa you got the wish.com bot version bro
     {user_message.replace("robot", user_name)}"""
   elif state == "counter":
+     state = "normal"
      return syllable_bot(user_message)
+  
   elif "run" in user_message:
     return "The FitnessGram Pacer Test is a multistage aerobic capacity test that progressively gets more difficult as it continues. The 20 meter pacer test will begin in 30 seconds. Line up at the start. The running speed starts slowly, but gets faster each minute after you hear this signal. [beep] A single lap should be completed each time you hear this sound. [ding] Remember to run in a straight line, and run as long as possible. The second time you fail to complete a lap before the sound, your test is over. The test will begin on the word start. On your mark, get ready, start."
   elif "give me something random" in user_message:
