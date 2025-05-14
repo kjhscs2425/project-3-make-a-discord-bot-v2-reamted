71c3a22b50d8a3b4988bddb31cb840966f9ea5f8
reamted <nroussos2028@kehillahstudent.org>
2025-05-14 12:04:09 -0700

    cool

--- ascii testing.py
+++ ascii testing.py
@@ -47,21 +47,37 @@ def haiku_bot(text):
     text = text.lower()
     text = re.sub(r'[^\w\s]','', text) # replace all non white space non text with a empty string
     total = 0
+    line_total = 0
     words = text.split()
     print(words)
+    target = 5 #target for the current line
+    line = 1 #current line
+    haiku = False
+    line1 = []
+    line2 = []
+    line3 = []
     for word in words:
-        #figuring out if something actually is a haiku
-        number = 0
+        
+        
         #line detector
         sylable = 0
         
         
         
         print(word)
+        if line == 1:
+            line1.append(word)
+        if line == 2:
+            line2.append(word)
+        if line == 3:
+            line3.append(word)
+        
         if word in exeptions:
             sylable = 1
         else:
             #throw away silent e
+            if word[-1] == 's':
+                word = word[:-1 ]
             if word[-1] == 'e':
                 word = word[:-1]
             #pad with a d
@@ -70,11 +86,37 @@ def haiku_bot(text):
             for consonant in consonants:
                 word = word.replace(consonant, 'd')
             #count vowel d's
-            sylable += word.count("ad") + word.count("ed") + word.count("id") + word.count("od") + word.count("ud") + word.count("yd")
+            sylable = word.count("ad") + word.count("ed") + word.count("id") + word.count("od") + word.count("ud") + word.count("yd")
         print(sylable)
         total += sylable
+        line_total += sylable
+        if line_total == target and line ==1:
+            target = 7
+            line = 2
+            line_total = 0
+        if line_total == target and line == 2:
+            target = 5
+            line = 3
+            line_total = 0
+        if line_total == target and line == 3:
+            haiku = True
+            
+        if total > 17:
+            print("Too long")
+            haiku = False
+            break
+            
     print(total)
+    print(haiku)
+    
+    #print(line1)
+    #print(line2)
+    #print(line3)
 
+    if haiku == True:
+        print(line1)
+        print(line2)
+        print(line3)
 
 
 def testing_bot(text):
@@ -135,7 +177,7 @@ def testing_bot(text):
 
 
 
-def word_spilter(text): 
+
     sylable = 0
     
     text += " "
@@ -164,4 +206,5 @@ def word_spilter(text):
 
 
 
-haiku_bot("i hate the french")
+#haiku_bot("I can detect your accidental haikus and format them in lines")
+haiku_bot("What we don’t notice we cannot love.  What lines love we seek to protect.")
